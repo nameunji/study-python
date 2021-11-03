2bb7ebe305a09b2a1735848ce9778c282241021d
nameunji <namji117@gmail.com>
2021-11-03 15:25:10 +0900

    drf section 2-10

--- django_rest_framework/first_api_django/onlinestore/products/views.py
+++ django_rest_framework/first_api_django/onlinestore/products/views.py
@@ -2,6 +2,41 @@ from django.http import JsonResponse
 
 from .models import Product, Manufacturer
 
+# Competency Assessment 1 - section 2-10
+"""
+1. given a primary key, the first endpoint will allow users to retrieve
+all the detailsabout the specific manufacturer (products included)
+2. The second endpoint will allow users to retrieve a list 
+of all the available active manufacturers in our online store
+"""
+def manufacturer_detail(request, pk):
+    try:
+        manufacturer = Manufacturer.objects.get(pk=pk)
+        manufacturer_products = manufacturer.products.all()
+        data = {"manufacturer": {
+            "name": manufacturer.name,
+            "city": manufacturer.location,
+            "active": manufacturer.active,
+            "products": list(manufacturer_products.values())
+        }}
+        response = JsonResponse(data)
+    except Manufacturer.DoesNotExist:
+        response = JsonResponse({
+            "error": {
+                "code": 404,
+                "message": "manufacturer not found!"
+            }
+        }, status=404)
+    return response
+
+
+def manufacturer_list(request):
+    manufacturers = Manufacturer.objects.filter(active=True)
+    data = {"manufacturers": list(manufacturers.values())}
+    response = JsonResponse(data)
+    return response
+
+
 # 2. Use JsonResponse
 def product_list(request):
     products = Product.objects.all()  # [:30]
@@ -34,16 +69,16 @@ def product_detail(request, pk):
 
 
 ### 1. pure Django view
-# from django.views.generic.detail import DetailView
-# from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-# from .models import Product, Manufacturer
+from .models import Product, Manufacturer
 
-# class ProductDetailView(DetailView):
-#     model = Product
-#     template_name = "products/product_detail.html"
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product_detail.html"
 
 
-# class ProductListView(ListView):
-#     model = Product
-#     template_name = "products/product_list.html"
+class ProductListView(ListView):
+    model = Product
+    template_name = "products/product_list.html"
